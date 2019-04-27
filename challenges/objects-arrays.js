d77d3564dd08fc551ea03bf8a7727d1b97726699
mikey <manicdesigner@gmail.com>
2019-04-27 12:55:48 -0400

    feat(objects..) .filter used in requst 3

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -247,15 +247,17 @@ console.log(lowerCase);
 The zoos are concenred about animals with a lower population count. Find out which animals have a population less than 5.
 
 */
-const largerPopulation = [];
-console.log(largerPopulation);
+let smallerPopulation = [];
+smallerPopulation = zooAnimals.filter(animalPop => (animalPop.population < 5));
+console.log(smallerPopulation);
 
 /* Request 4: .reduce() 
 
 The zoos need to know their total animal population across the United States.  Find the total population from all the zoos using the .reduce() method.
 
 */
-const populationTotal = 0;
+let populationTotal = 0;
+
 console.log(populationTotal);
 
 
